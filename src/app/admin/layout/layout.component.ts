07fabf1434331f111a03c58b4156b73232d00396
MertGuney <guneeymert@gmail.com>
2023-01-18 11:38:00 +0300

    AlertifyJs library added and costomized

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AlertifyService, MessageType, Position } from './../../services/admin/alertify.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  
+  constructor(private alertify: AlertifyService) { }
+
+  ngOnInit(): void {
+    this.alertify.message("Alertify Service Test", {
+      messageType: MessageType.Success,
+      delay: 5,
+      position: Position.TopRight,
+      dismissOther: true
+    })
+  }
 
 }
